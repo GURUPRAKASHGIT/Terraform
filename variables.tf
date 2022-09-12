19217d839e5af853414cd68637dc3e6dd9d84c26
rajkumar2289 <82703419+rajkumar2289@users.noreply.github.com>
2022-09-12 15:24:49 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -6,3 +6,26 @@ variable "instance_name" {
   type = list
   default = ["Dev", "Staging","prod"]
 }
+
+variable "ami" { 
+default = ""
+}
+
+variable "instance_type" {
+    default = "t2.micro"
+  }
+
+variable "key_name" {
+   default = "ohio"
+}       
+                      
+variable "associate_public_ip_address"  {
+    type = bool
+    description = "(optional) describe your variable"
+    default =  true
+} 
+ variable "instance_count"  {
+    type = number
+    description = "(optional) describe your variable"
+    default = 3
+ }  
