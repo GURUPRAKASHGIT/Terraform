7d37f9b4f418b4c2c5b75d9fc19456851ce545c7
rajkumar2289 <82703419+rajkumar2289@users.noreply.github.com>
2022-09-12 14:38:03 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -10,10 +10,18 @@ resource "aws_instance" "app-dev" {
    instance_type = "t2.micro"
    key_name = "ec2-key"
    tags = local.common_tags
+  count = 3
 }
 
 resource "aws_ebs_volume" "db_ebs" {
   availability_zone = "us-east-2c"
   size              = 8
   tags = local.common_tags
+}
+
+resource "aws_instance" "webinstance" {
+   ami = "ami-082b5a644766e0e6f"
+   instance_type = var.instance_type
+   count = 3
+  tags = var.instance_name[count.index]
 }
